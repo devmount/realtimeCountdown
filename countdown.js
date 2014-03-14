fb7e833802476782b373c842cdaa61287726eb2a
devmount <hpdesigner@web.de>
2014-03-14 17:36:16 +0100

    improved: comments and indent

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-// Ziel-Datum in MEZ
+// target date (MEZ)
 var zielDatum;
 
 // output strings
@@ -44,123 +44,126 @@ var str_second = '';
 var id = "0";
 
 function initCountdown(languagelabels, jahr, monat, tag, stunde, minute, sekunde, text, beforecd, aftercd, hideyear, hidemonth, hideday, hidehour, hideminute, hidesecond, hideleadingzero, num) {
-	languagelabels = languagelabels.split(" ");
-	label_year = languagelabels[0];
-	label_years = languagelabels[1];
-	label_month = languagelabels[2];
-	label_months = languagelabels[3];
-	label_day = languagelabels[4];
-	label_days = languagelabels[5];
-	label_hour = languagelabels[6];
-	label_hours = languagelabels[7];
-	label_minute = languagelabels[8];
-	label_minutes = languagelabels[9];
-	label_second = languagelabels[10];
-	label_seconds = languagelabels[11];
-	label_and = languagelabels[12];
-	zielDatum = new Date(jahr, monat-1, tag, stunde, minute, sekunde);
-	aftercount = text;
-	beforecountdown = beforecd;
-	aftercountdown = aftercd;
-	hide_year = hideyear;
-	hide_month = hidemonth;
-	hide_day = hideday;
-	hide_hour = hidehour;
-	hide_minute = hideminute;
-	hide_second = hidesecond;
-	hide_leading_zero = hideleadingzero;
-	id = num;
-	countdown();
+    languagelabels    = languagelabels.split(" ");
+    label_year        = languagelabels[0];
+    label_years       = languagelabels[1];
+    label_month       = languagelabels[2];
+    label_months      = languagelabels[3];
+    label_day         = languagelabels[4];
+    label_days        = languagelabels[5];
+    label_hour        = languagelabels[6];
+    label_hours       = languagelabels[7];
+    label_minute      = languagelabels[8];
+    label_minutes     = languagelabels[9];
+    label_second      = languagelabels[10];
+    label_seconds     = languagelabels[11];
+    label_and         = languagelabels[12];
+    zielDatum         = new Date(jahr, monat-1, tag, stunde, minute, sekunde);
+    aftercount        = text;
+    beforecountdown   = beforecd;
+    aftercountdown    = aftercd;
+    hide_year         = hideyear;
+    hide_month        = hidemonth;
+    hide_day          = hideday;
+    hide_hour         = hidehour;
+    hide_minute       = hideminute;
+    hide_second       = hidesecond;
+    hide_leading_zero = hideleadingzero;
+    id                = num;
+    countdown();
 }
 
 function countdown() {
 
-	var startDatum = new Date(); // Aktuelles Datum
+    var startDatum = new Date(); // current date
 
-	// Countdown berechnen und anzeigen, bis Ziel-Datum erreicht ist
-	if(startDatum < zielDatum)  {
-		var jahre = 0, monate = 0, tage = 0, stunden = 0, minuten = 0, sekunden = 0;
+    // evaluate countdown until target date
+    if(startDatum < zielDatum) {
+        var jahre = 0, monate = 0, tage = 0, stunden = 0, minuten = 0, sekunden = 0;
 
-		// Jahre
-		while(startDatum < zielDatum) {
-			jahre++;
-			startDatum.setFullYear(startDatum.getFullYear() + 1);
-		}
-		startDatum.setFullYear(startDatum.getFullYear() - 1);
-		jahre--;
+        // years
+        while(startDatum < zielDatum) {
+            jahre++;
+            startDatum.setFullYear(startDatum.getFullYear() + 1);
+        }
+        startDatum.setFullYear(startDatum.getFullYear() - 1);
+        jahre--;
 
-		// Monate
-		while(startDatum < zielDatum) {
-			monate++;
-			startDatum.setMonth(startDatum.getMonth() + 1);
-		}
-		startDatum.setMonth(startDatum.getMonth() - 1);
-		monate--;
+        // months
+        while(startDatum < zielDatum) {
+            monate++;
+            startDatum.setMonth(startDatum.getMonth() + 1);
+        }
+        startDatum.setMonth(startDatum.getMonth() - 1);
+        monate--;
 
-		// Tage
-		while(startDatum.getTime() + (24*60*60*1000) < zielDatum) {
-			tage++;
-			startDatum.setTime(startDatum.getTime() + (24*60*60*1000));
-		}
+        // days
+        while(startDatum.getTime() + (24*60*60*1000) < zielDatum) {
+            tage++;
+            startDatum.setTime(startDatum.getTime() + (24*60*60*1000));
+        }
 
-		// Stunden
-		stunden = Math.floor((zielDatum - startDatum)/(60*60*1000));
-		startDatum.setTime(startDatum.getTime() + stunden*60*60*1000);
+        // hours
+        stunden = Math.floor((zielDatum - startDatum)/(60*60*1000));
+        startDatum.setTime(startDatum.getTime() + stunden*60*60*1000);
 
-		// Minuten
-		minuten = Math.floor((zielDatum - startDatum)/(60*1000));
-		startDatum.setTime(startDatum.getTime() + minuten*60*1000);
+        // minutes
+        minuten = Math.floor((zielDatum - startDatum)/(60*1000));
+        startDatum.setTime(startDatum.getTime() + minuten*60*1000);
 
-		// Sekunden
-		sekunden = Math.floor((zielDatum - startDatum)/1000);
+        // seconods
+        sekunden = Math.floor((zielDatum - startDatum)/1000);
 
-		//Anzeige formatieren
+        // initialize output string
+        var arrstr = new Array();
+        var str = '';
 
-		var arrstr = new Array();
-		var str = '';
-		var show_year = !hide_year & !(hide_leading_zero & jahre == 0);
-		var show_month = !hide_month & !(hide_leading_zero & jahre == 0 & monate == 0);
-		var show_day = !hide_day & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0);
-		var show_hour = !hide_hour & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0 & stunden == 0);
-		var show_minute = !hide_minute & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0 & stunden == 0 & minuten == 0);
-		var show_second = !hide_second;
+        // get booleans wether to show specific element
+        var show_year = !hide_year & !(hide_leading_zero & jahre == 0);
+        var show_month = !hide_month & !(hide_leading_zero & jahre == 0 & monate == 0);
+        var show_day = !hide_day & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0);
+        var show_hour = !hide_hour & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0 & stunden == 0);
+        var show_minute = !hide_minute & !(hide_leading_zero & jahre == 0 & monate == 0 & tage == 0 & stunden == 0 & minuten == 0);
+        var show_second = !hide_second;
 
-		if (show_year) {
-			(jahre != 1) ? str_year = jahre + " " + label_years : str_year = jahre + " " + label_year;
-			arrstr.splice(0,0,str_year);
-		};
-		if (show_month) {
-			(monate != 1) ? str_month = monate + " " + label_months : str_month = monate + " " + label_month;
-			arrstr.splice(0,0,str_month);
-		};
-		if (show_day) {
-			(tage != 1) ? str_day = tage + " " + label_days : str_day = tage + " " + label_day;
-			arrstr.splice(0,0,str_day);
-		};
-		if (show_hour) {
-			(stunden != 1) ? str_hour = stunden + " " + label_hours : str_hour = stunden + " " + label_hour;
-			arrstr.splice(0,0,str_hour);
-		};
-		if (show_minute) {
-			(minuten != 1) ? str_minute = minuten + " " + label_minutes : str_minute = minuten + " " + label_minute;
-			arrstr.splice(0,0,str_minute);
-		};
-		if (show_second) {
-			if(sekunden < 10) sekunden = "0" + sekunden;
-			(sekunden != 1) ? str_second = sekunden + " " + label_seconds : str_second = sekunden + " " + label_second;
-			arrstr.splice(0,0,str_second);
-		 };
+        // format output
+        if (show_year) {
+            (jahre != 1) ? str_year = jahre + " " + label_years : str_year = jahre + " " + label_year;
+            arrstr.splice(0,0,str_year);
+        };
+        if (show_month) {
+            (monate != 1) ? str_month = monate + " " + label_months : str_month = monate + " " + label_month;
+            arrstr.splice(0,0,str_month);
+        };
+        if (show_day) {
+            (tage != 1) ? str_day = tage + " " + label_days : str_day = tage + " " + label_day;
+            arrstr.splice(0,0,str_day);
+        };
+        if (show_hour) {
+            (stunden != 1) ? str_hour = stunden + " " + label_hours : str_hour = stunden + " " + label_hour;
+            arrstr.splice(0,0,str_hour);
+        };
+        if (show_minute) {
+            (minuten != 1) ? str_minute = minuten + " " + label_minutes : str_minute = minuten + " " + label_minute;
+            arrstr.splice(0,0,str_minute);
+        };
+        if (show_second) {
+            if(sekunden < 10) sekunden = "0" + sekunden;
+            (sekunden != 1) ? str_second = sekunden + " " + label_seconds : str_second = sekunden + " " + label_second;
+            arrstr.splice(0,0,str_second);
+         };
 
-		for (var i = arrstr.length - 1; i >= 0; i--) {
-			str = str + arrstr[i];
-			if (i > 1) { str = str + ", "; };
-			if (i == 1) { str = str + " " + label_and + " "; };
-		};
+        for (var i = arrstr.length - 1; i >= 0; i--) {
+            str = str + arrstr[i];
+            if (i > 1) { str = str + ", "; };
+            if (i == 1) { str = str + " " + label_and + " "; };
+        };
 
-		// Anzeige aktualisieren
-		document.getElementById(id).innerHTML = beforecountdown + str + aftercountdown;
-		setTimeout('countdown()',200);
-	}
-	// after countdown
-	else document.getElementById(id).innerHTML = aftercount;
+        // Anzeige aktualisieren
+        document.getElementById(id).innerHTML = beforecountdown + str + aftercountdown;
+        setTimeout('countdown()',200);
+    } else {
+        // show text for after count
+        document.getElementById(id).innerHTML = aftercount;
+    }
 }
